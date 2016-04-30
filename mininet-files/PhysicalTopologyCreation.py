6f464fba4b87a0069b9cdf003c9bc95faa592e35
Kausik Subramanian <sskausik08@gmail.com>
2016-04-30 14:49:53 -0500

    Updated mininet for testing

--- mininet-files/PhysicalTopologyCreation.py
+++ mininet-files/PhysicalTopologyCreation.py
@@ -15,7 +15,16 @@ setIP()
 """
 
 from mininet.topo import Topo
+from mininet.net import Mininet
+from mininet.node import CPULimitedHost
 from mininet.link import TCLink
+from mininet.util import dumpNodeConnections
+from mininet.log import setLogLevel
+from mininet.node import OVSSwitch, Controller, RemoteController
+from mininet.cli import CLI
+
+import time
+
 
 class PhyTopo( Topo ):
 	"Create Physical Topology based on specifications."
@@ -27,6 +36,7 @@ class PhyTopo( Topo ):
 		Topo.__init__( self )
 
 
+		print "Topology Instantiation"
 		#Add switches
 		f1 = open("phy-switches", 'r')
 		switch_desc = f1.readlines()
@@ -59,5 +69,41 @@ class PhyTopo( Topo ):
 			self.addLink( switchMap[lineArr[0]],  switchMap[lineArr[1]], bw= float(lineArr[2]), max_queue_size=500) 
 	
 
+#topos = { 'PhyTopo': ( lambda: PhyTopo() ) }
 
-topos = { 'PhyTopo': ( lambda: PhyTopo() ) }
+def perfTest():
+	"Create network and run simple performance test"
+	print "Here"
+	scheduler = RemoteController( 'scheduler', ip='192.168.56.1', port=6633 )
+	topo = PhyTopo()
+	net = Mininet(topo=topo, 
+				  host=CPULimitedHost, link=TCLink, build=False)
+	net.addController(scheduler)
+	net.build()
+
+
+	# Set MAC addresses 
+	f1 = open("phy-hosts", 'r')
+	host_desc = f1.readlines()
+
+	for line in host_desc:
+		lineArr = line.split()
+		host = net.get(lineArr[0])
+		ip = lineArr[2].split(".")
+		ethstr = str("00:00:00:00:00:") + ip[3]
+		host.setMAC(ethstr)
+		
+
+	net.start()
+	time.sleep(15)
+	h1, h4 = net.get('h1', 'h4')
+	net.iperf((h1, h4))
+	net.pingAll()
+	net.pingAll()
+	
+
+
+if __name__ == '__main__':
+	setLogLevel('info')
+	perfTest()
+
