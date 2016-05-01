422a38e78315f7fbd4aab3833ea9bd144d905c67
sturdevant <sturdevant2@wisc.edu>
2016-04-30 21:46:38 -0500

    now cdf looks at per flow

--- data/pt1/cdf.py
+++ data/pt1/cdf.py
@@ -4,28 +4,37 @@ import numpy as np
 from matplotlib import pyplot as plt
 
 for fname in glob.glob("*.*.*.*"):
-    times = []
+    flows = {}
     with open(fname, 'r') as f:
         for line in f:
             ln = line.split()
             time = float(ln[1])
-            times.append(time)
+            srcip = ln[2]
+            dstip = ln[4]
+            srcprt = ln[7]
+            dstprt = ln[9]
+            key = srcip + ":" + srcprt + " -> " + dstip + ":" + dstprt
+            if key not in flows.keys():
+                flows[key] = [] 
+            flows[key].append(time)
 
-    # Sort just in case (shouldn't make a difference)
-    times.sort()
+    for key in flows.keys():
+        times = flows[key]
+        # Sort just in case (shouldn't make a difference)
+        times.sort()
 
-    percents = []
-    cumulative = 0
-    for time in times:
-        cumulative += time
-        percents.append(cumulative)
+        percents = []
+        cumulative = 0
+        for time in times:
+            cumulative += time
+            percents.append(cumulative)
 
-    # Normalize
-    for i in range(len(percents)):
-        percents[i] = percents[i] / cumulative
+        # Normalize
+        for i in range(len(percents)):
+            percents[i] = percents[i] / cumulative
 
-    plt.step(times, percents)
-    plt.title(fname)
-    plt.xlabel("Time (s)")
-    plt.ylabel("% of packets")
-    plt.show()
+        plt.step(times, percents)
+        plt.title(key)
+        plt.xlabel("Time (s)")
+        plt.ylabel("% of packets (t <= T)")
+        plt.show()
